a045a8297561d51145f3be4519d2491925edecdc
Dennis Empirio <dennis.empirio@gmail.com>
2023-01-26 19:40:08 +0100

    aggiunta stipendio in impiegato

--- index.php
+++ index.php
@@ -13,14 +13,77 @@
             $this->luogo_nascita = $luogo_nascita;
             $this->codice_fiscale = $codice_fiscale;  
         }
-    }
-    class Impiegato extends Persona {
-        private $data_assunzione;
-        public function __construct($nome, $cognome, $data_nascita, $luogo_nascita, $codice_fiscale, $data_assunzione) {
-            parent::__construct($nome, $cognome, $data_nascita, $luogo_nascita, $codice_fiscale);
-            $this->data_assunzione = $data_assunzione;
+
+        // tabella 
+        public function getHTML(){
+            echo     "<table>
+                        <tr>
+                            <td>Nome:</td>
+                            <td>".$this->nome."</td>
+                        </tr>
+                        <tr>
+                            <td>Cognome:</td>
+                            <td>".$this->cognome."</td>
+                        </tr>
+                        <tr>
+                            <td>Data di nascita:</td>
+                            <td>".$this->data_nascita."</td>
+                        </tr>
+                        <tr>
+                            <td>Luogo di nascita:</td>
+                            <td>".$this->luogo_nascita."</td>
+                        </tr>
+                        <tr>
+                            <td>Codice Fiscale:</td>
+                            <td>".$this->codice_fiscale."</td>
+                        </tr>
+                    </table>";
         }
     }
+    
+    class Impiegato extends Persona {
+        private $data_assunzione;
+        private $stipendio;
+        public function __construct($nome, $cognome, $data_nascita, $luogo_nascita, $codice_fiscale, $data_assunzione, $stipendio_mensile, $tredicesima, $quattordicesima) {
+            parent::__construct($nome, $cognome, $data_nascita, $luogo_nascita, $codice_fiscale);
+            $this->data_assunzione = $data_assunzione;
+            $this->stipendio = new Stipendio($stipendio_mensile, $tredicesima, $quattordicesima);
+        }
+    
+        public function getHTML() {
+            echo     "<table>
+                        <tr>
+                            <td>Nome:</td>
+                            <td>".$this->nome."</td>
+                        </tr>
+                        <tr>
+                            <td>Cognome:</td>
+                            <td>".$this->cognome."</td>
+                        </tr>
+                        <tr>
+                            <td>Data di nascita:</td>
+                            <td>".$this->data_nascita."</td>
+                        </tr>
+                        <tr>
+                            <td>Luogo di nascita:</td>
+                            <td>".$this->luogo_nascita."</td>
+                        </tr>
+                        <tr>
+                            <td>Codice Fiscale:</td>
+                            <td>".$this->codice_fiscale."</td>
+                        </tr>
+                        <tr>
+                            <td>Data di assunzione:</td>
+                            <td>".$this->data_assunzione."</td>
+                        </tr>
+                        <tr>
+                            <td>Stipendio annuale:</td>
+                            <td>".$this->stipendio->stipendio_annuale()."</td>
+                        </tr>
+                    </table>";
+        }
+    }
+    
     class Capo extends Persona {
         private $bonus;
         private $dividendo;
@@ -30,22 +93,30 @@
             $this->$dividendo = $dividendo;
         }
     }
+
     class Stipendio {
-        private $impiegato;
-        private $importo;
-        private $data_pagamento;
+        protected $stipendio_mensile;
+        protected $tredicesima;
+        protected $quattordicesima;
     
-        public function __construct($impiegato, $importo, $data_pagamento) {
-            $this->impiegato = $impiegato;
-            $this->importo = $importo;
-            $this->data_pagamento = $data_pagamento;
+        public function __construct($stipendio_mensile, $tredicesima, $quattordicesima) {
+            $this->stipendio_mensile = $stipendio_mensile;
+            $this->tredicesima = $tredicesima;
+            $this->quattordicesima = $quattordicesima;
         }
-        public function stipendioAnnuale() {
-            $tredicesima = $this->importo / 12;
-            $quattordicesima = $this->importo / 12;
-            return 12 * $this->importo + $tredicesima + $quattordicesima;
+    
+        public function stipendio_annuale() {
+            $stipendio_annuale = $this->stipendio_mensile * 12 + ($this->tredicesima ? $this->stipendio_mensile : 0) + ($this->quattordicesima ? $this->stipendio_mensile : 0);
+            return $stipendio_annuale;
         }
     }
+
+    $persona = new Persona("Mario", "Rossi", "01/01/2000", "Roma", "MRARSS00A01H501Q");
+    echo $persona->getHTML();
+    
+    $impiegato = new Impiegato("Mario", "Rossi", "01/01/2000", "Roma", "MRARSS00A01H501Q", "01/01/2022", 2000, true, true);
+    $impiegato->getHTML();
+
 ?>
 
 
